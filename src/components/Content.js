5fc2147e24266dd69713909905206826cf87720c
BintReina <omobowalerohma1998@gmail.com>
2022-10-28 16:06:23 +0100

    shows the image hover effect and the share icons

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "../styles/content.css";
-import veil from "../images/veil.png";
+import girl from "../images/girl.png";
 import Socials from "./Socials";
 import Icons from "./Icons";
-import share from "../images/share.png";
+import sharemobile from "../images/share.png";
+import shareweb from "../images/shareweb.png";
+
+import camera from "../images/camera.png";
 export default function Content() {
   return (
     <>
-      <img alt="the share icon" src={share} className='share' />
+      <img alt="the share icon" src={sharemobile} className="share sharemobile" />
+      <img alt="the share icon" src={shareweb} className="share shareweb" />
+
       <div className="img--container">
-        <img id="profile__img" alt="a-veiled-lady" src={veil} />
+        <img id="profile__img" alt="a-lady" src={girl} />
+        <div className="overlay">
+          <img src={camera} alt="a camera icon" className="camera" />
+        </div>
       </div>
       <p id="twitter">@rohmaabdulfatt1</p>
       <p id="slack">veiled-coder</p>
