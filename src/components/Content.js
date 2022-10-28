455c9929be951ae5028d6fe41fbeb7ff4635046f
BintReina <omobowalerohma1998@gmail.com>
2022-10-28 10:03:23 +0100

    show the responsive design interface

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,11 +3,13 @@ import "../styles/content.css";
 import veil from "../images/veil.png";
 import Socials from "./Socials";
 import Icons from "./Icons";
+import share from "../images/share.png";
 export default function Content() {
   return (
     <>
+      <img alt="the share icon" src={share} className='share' />
       <div className="img--container">
-        <img id="profile__img" alt="a-veiled-lady" src={veil}></img>
+        <img id="profile__img" alt="a-veiled-lady" src={veil} />
       </div>
       <p id="twitter">@rohmaabdulfatt1</p>
       <p id="slack">veiled-coder</p>
--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,9 +1,12 @@
 import React from "react";
 import slack from ".././images/slack.png";
+import git from ".././images/git.png";
+
 function Icons() {
   return (
-    <div>
-      <img alt="slack--img" src={slack}></img>
+    <div className="icon--container">
+      <img alt="slack--img" src={slack} className='icons'></img>
+      <img alt="slack--img" src={git} className='icons'></img>
     </div>
   );
 }
