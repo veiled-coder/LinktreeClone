c3b9616bf05e5039fef30592aacb3a8234bc57ef
BintReina <omobowalerohma1998@gmail.com>
2022-11-04 08:24:28 +0100

    routes:added the routes to the contact page

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -37,7 +37,7 @@ export default function Socials() {
         />
       </a>
       <Link to="/contact">
-        <Buttons name="Contact Me" />
+        <Buttons name="Contact Me" id="contact" />
       </Link>
     </section>
   );
